5182b2b02f29ed2258af71312555a80170449401
Loic M <loic@Loics-Mac.local>
2019-12-31 04:24:32 -0800

    install gunicorn

--- app.py
+++ app.py
@@ -76,19 +76,19 @@ def username_taken(username):
 # -------- Login ------------------------------------------------------------- #
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    # if not session.get('logged_in'):
-    form = forms.LoginForm(request.form)
-    if request.method == 'POST':
-        username = request.form['username'].lower()
-        password = request.form['password']
-        if form.validate():
-            if credentials_valid(username, password):
-                session['logged_in'] = True
-                session['username'] = username
-                return json.dumps({'status': 'Login successful'})
-            return json.dumps({'status': 'Invalid user/pass'})
-        return json.dumps({'status': 'Both fields required'})
-    return render_template('login.html', form=form)
+    if not session.get('logged_in'):
+        form = forms.LoginForm(request.form)
+        if request.method == 'POST':
+            username = request.form['username'].lower()
+            password = request.form['password']
+            if form.validate():
+                if credentials_valid(username, password):
+                    session['logged_in'] = True
+                    session['username'] = username
+                    return json.dumps({'status': 'Login successful'})
+                return json.dumps({'status': 'Invalid user/pass'})
+            return json.dumps({'status': 'Both fields required'})
+        return render_template('login.html', form=form)
     user = get_user()
     return render_template('home.html', user=user)
 
